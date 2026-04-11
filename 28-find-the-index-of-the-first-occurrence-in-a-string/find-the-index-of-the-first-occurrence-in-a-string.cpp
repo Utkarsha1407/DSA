cf54756b8f0b1972a2d7704e399ffe659061b4e8
Utkarsha Chougule <utkarshachougule04@gmail.com>
2026-04-11 14:33:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (73.16%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int strStr(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+
+          if (n == 0) return 0;  // edge case
+
+
+        for(int i=0; i<=m-n; i++) {
+            for(int j=0; j<n; j++) {
+                if(s1[i+j]!=s2[j]) 
+                    break;
+
+                if(j==n-1) 
+                     return i;
+                
+                }
+            
+            }
+
+        
+        return -1;
+    }
+};
