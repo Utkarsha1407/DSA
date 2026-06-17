3cfe78c9b22e4970999c537244f46fd038e65100
Utkarsha Chougule <utkarshachougule04@gmail.com>
2026-06-17 11:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (18.72%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1;
-        int first = -1, last = -1;
+        int start =0, end = nums.size()-1, first = -1, last = -1, mid;
+        
+        while(start<=end) {
+            mid = start+(end-start)/2;
 
-        // FIRST POSITION
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
+            if(nums[mid]==target) {
                 first = mid;
-                end = mid - 1;   // move left
+                end = mid-1;
+
             }
-            else if (nums[mid] < target) {
-                start = mid + 1;
+            else if(nums[mid]<target) {
+                start = mid+1;
             }
             else {
-                end = mid - 1;
-            }
+                end = mid-1;
+            } 
         }
 
-        // LAST POSITION
-        start = 0;
-        end = nums.size() - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
+         start =0, end = nums.size()-1;
+        while(start<=end) {
+            mid = start+(end-start)/2;
+
+            if(nums[mid]==target) {
                 last = mid;
-                start = mid + 1;  // move right
+                start = mid +1;
             }
-            else if (nums[mid] < target) {
-                start = mid + 1;
-            }
-            else {
-                end = mid - 1;
-            }
-        }
+            else if(nums[mid]>target) 
+            end = mid-1;
 
+            else
+            start = mid+1;
+        }
         return {first, last};
-    }
+            }
 };
