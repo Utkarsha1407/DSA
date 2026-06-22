13cafb432b1f709e64635bbd3f9b2455395a9d5c
Utkarsha Chougule <utkarshachougule04@gmail.com>
2026-06-22 22:24:13 +0530

    Time: 3 ms (25.99%) | Memory: 7.5 MB (94.11%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans;
+        int n = address.size();
+
+        for(int i=0; i<n; i++) {
+            if(address[i]=='.') {
+                ans = ans + "[.]";
+            }
+            else {
+                ans = ans + address[i];
+            }
+
+        }
+        return ans;
+    }
+};
