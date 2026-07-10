7d2c04511db9cfcd07baf55b30701f97a3aaf03e
Utkarsha Chougule <utkarshachougule04@gmail.com>
2026-07-10 15:44:57 +0530

    Time: 4 ms (67.09%) | Memory: 11.6 MB (36.24%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        if(s.size() < p.size()) return {};
         
-        vector<int> si(26,0), pi(26,0);
-        int right = 0, left = 0;
-        
-        // First window
+        if(s.size()<p.size()) 
+        return {};
+
+        vector<int>si(26,0), pi(26,0);
+        int left=0, right=0;
+
         while(right < p.size()) {
-            si[s[right] - 'a']++;
-            pi[p[right] - 'a']++;
+            si[s[right]-'a']++;
+            pi[p[right]-'a']++;
             right++;
         }
-        
-        vector<int> ans;
-        
-        while(right < s.size()) {
-            
-            if(si == pi)
+         
+         vector<int>ans;
+        while(right<s.size()) {
+            if(si==pi) 
                 ans.push_back(left);
-            
-            // Slide window
-            si[s[left] - 'a']--;
+        
+
+            si[s[left]-'a']--;
             left++;
-            
-            si[s[right] - 'a']++;
+
+            si[s[right]-'a']++;
             right++;
         }
-        
-        // Last window check
-        if(si == pi)
+
+        if(si==pi) 
             ans.push_back(left);
         
+    
         return ans;
     }
 };
