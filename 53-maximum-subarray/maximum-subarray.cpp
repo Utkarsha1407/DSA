23cccbfec2bb8e4afbdbfc9b36f39b3912963942
Utkarsha Chougule <utkarshachougule04@gmail.com>
2026-07-19 20:30:37 +0530

    Refactor maxSubArray function for clarity and efficiency

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-     
-    int currentSum = nums[0];
-    int maxSum = nums[0];
+        
+        int sum = 0, maxsum = nums[0];
 
-    for (int i = 1; i < nums.size(); i++) {
-        currentSum = max(nums[i], currentSum + nums[i]);
-        maxSum = max(maxSum, currentSum);
+        for(int i=0; i<nums.size(); i++) {
+            sum+=nums[i];
+
+            maxsum = max(maxsum, sum);
+        
+
+        if(sum < 0) {                 //If the current subarray has a negative sum, don't continue it. Start a new subarray from the next element.
+            sum=0;
+        }
+        }
+        return maxsum;
     }
-
-    return maxSum;
-} 
-    
+        
 };
