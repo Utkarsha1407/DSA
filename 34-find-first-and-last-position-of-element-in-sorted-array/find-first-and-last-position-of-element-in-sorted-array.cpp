3f4cb7ff6fbd8ba7872425a5285bb750e088bd2a
Utkarsha Chougule <utkarshachougule04@gmail.com>
2026-08-07 16:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (20.21%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start =0, end = nums.size()-1, first = -1, last = -1, mid;
-        
+        int start=0, end=nums.size()-1, mid, first=-1, last=-1;
+
         while(start<=end) {
-            mid = start+(end-start)/2;
+            mid= start+(end-start)/2;
 
             if(nums[mid]==target) {
-                first = mid;
-                end = mid-1;
-
+                first=mid;
+                end=mid-1;
             }
+
             else if(nums[mid]<target) {
                 start = mid+1;
             }
             else {
-                end = mid-1;
-            } 
+                end=mid-1;
+            }
         }
 
-         start =0, end = nums.size()-1;
+         start=0, end= nums.size()-1;
         while(start<=end) {
-            mid = start+(end-start)/2;
-
-            if(nums[mid]==target) {
-                last = mid;
-                start = mid +1;
+            mid=start+(end-start)/2;
+        
+        if(nums[mid]==target) {
+                last=mid;
+                start=mid+1;
             }
-            else if(nums[mid]>target) 
-            end = mid-1;
 
-            else
-            start = mid+1;
+            else if(nums[mid]<target) {
+               start = mid+1;
+            }
+            else {
+                end=mid-1;
+            }
         }
-        return {first, last};
-            }
+            
+        
+
+        return {first,last};
+    }
 };
